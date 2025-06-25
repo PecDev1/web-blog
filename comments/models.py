b2e55bf4eb7806b5f8dd34039021b68b31b7a0d8
PecDev1 <abdumajidovazizbek411@gmail.com>
2025-06-25 21:28:40 +0500

    error was fixed

--- comments/models.py
+++ comments/models.py
@@ -1,17 +1,17 @@
-
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+from datetime import datetime  # Use standard library
 
-from django.utils import timezone
 from news.models import News
 
 class Comment(models.Model):
     news = models.ForeignKey(News, related_name='comments', on_delete=models.CASCADE)
-    content = models.TextField(max_length=500, null=True)  # Use 'content' for clarity
+    content = models.TextField(max_length=500, null=True)
     author = models.ForeignKey(get_user_model(), related_name='comments', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)  # Set default to current time
-    def __str__(self):  # Corrected str method
+    created_at = models.DateTimeField(default=datetime.now)  # Use datetime.now as default
+
+    def __str__(self):
         return f'Comment by {self.author} on {self.news}'
 
     def get_absolute_url(self):
